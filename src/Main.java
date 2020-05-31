8bd49b1830b4225c3467420a05b54c9427233cf8
Maxim Boldyrev <maxikb@mail.ru>
2020-05-31 17:54:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 750;
+        int miles = 0;
+        miles = ticketPrice / 20;
+        System.out.println("Стоимость билета: "+ ticketPrice);
+        System.out.println("Вам начислено миль: " + miles);
+
+    }
+}
