4943fd2117f593c645d438a16a2b926052775b70
Maxim Boldyrev <maxikb@mail.ru>
2020-06-04 23:19:51 +0300

    calc added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 750;
-        int miles = 0;
-        miles = ticketPrice / 20;
-        System.out.println("Стоимость билета: "+ ticketPrice);
-        System.out.println("Вам начислено миль: " + miles);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
